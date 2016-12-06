d9deaeb181163c129477b1a7d6017b5a3d616b04
Ark4ne <gui.allegret@gmail.com>
2016-12-06 11:48:38 +0100

    fix(Config) : add missing file

---
+++ config/app.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  /*
+   +---------------------------------------------------------------
+   | Application base uri
+   +---------------------------------------------------------------
+   |
+   | This value define the application base uri.
+   | It is useful for router & url service.
+   */
+  'base_uri' => '/',
+];
