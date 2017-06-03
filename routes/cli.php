02136535be3f1683e010bfc143d47b87d0a9eefd
Ark4ne <gui.allegret@gmail.com>
2017-06-04 01:46:39 +0200

    route cli : fix command pattern

--- routes/cli.php
+++ routes/cli.php
@@ -15,7 +15,7 @@ use Neutrino\Support\Facades\Router;
 Router::add('some', [
     'task' => \App\Kernels\Cli\Tasks\SomeTask::class
 ]);
-Router::add('some:test {param_1} {param_2})', [
+Router::add('some:test {param_1} {param_2}', [
   'task'    => \App\Kernels\Cli\Tasks\SomeTask::class,
   'action'  => 'test',
 ]);
