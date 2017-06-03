9e40382586ebf1b3ae90e20ec98dd9a017c0bf63
Ark4ne <gui.allegret@gmail.com>
2017-06-03 04:22:11 +0200

    feat(Error\Handler): remove unneeded try catch

--- public/index.php
+++ public/index.php
@@ -1,31 +1,21 @@
 <?php
 
-try {
-    /*--------------------------------------------------------------*/
-    /* Make Application                                             */
-    /*--------------------------------------------------------------*/
+/*--------------------------------------------------------------*/
+/* Make Application                                             */
+/*--------------------------------------------------------------*/
 
-    /** @var \Neutrino\Foundation\Bootstrap $bootstrap */
-    $bootstrap = require_once __DIR__ . '/../bootstrap/app.php';
+/** @var \Neutrino\Foundation\Bootstrap $bootstrap */
+$bootstrap = require_once __DIR__ . '/../bootstrap/app.php';
 
-    /** @var \App\Kernels\Http\Kernel $kernel */
-    $kernel = $bootstrap->make(App\Kernels\Http\Kernel::class);
+/** @var \App\Kernels\Http\Kernel $kernel */
+$kernel = $bootstrap->make(App\Kernels\Http\Kernel::class);
 
-    /**
-     * Handle the request
-     */
-    $response = $kernel->handle();
+/**
+ * Handle the request
+ */
+$response = $kernel->handle();
 
-    /**
-     * Send the response.
-     */
-    $response->send();
-
-    /**
-     * Terminate the kernel
-     */
-    $kernel->terminate();
-} catch (\Exception $e) {
-    echo $e->getMessage() . '<br>';
-    echo '<pre>' . $e->getTraceAsString() . '</pre>';
-}
+/**
+ * Send the response.
+ */
+$response->send();
--- public/micro.php
+++ public/micro.php
@@ -1,26 +1,15 @@
 <?php
 
-try {
-    /*--------------------------------------------------------------*/
-    /* Make Application                                             */
-    /*--------------------------------------------------------------*/
+/*--------------------------------------------------------------*/
+/* Make Application                                             */
+/*--------------------------------------------------------------*/
+/** @var \Neutrino\Foundation\Bootstrap $bootstrap */
+$bootstrap = require_once __DIR__ . '/../bootstrap/app.php';
 
-    /** @var \Neutrino\Foundation\Bootstrap $bootstrap */
-    $bootstrap = require_once __DIR__ . '/../bootstrap/app.php';
+/** @var \App\Kernels\Micro\Kernel $kernel */
+$kernel = $bootstrap->make(\App\Kernels\Micro\Kernel::class);
 
-    /** @var \App\Kernels\Micro\Kernel $kernel */
-    $kernel = $bootstrap->make(\App\Kernels\Micro\Kernel::class);
-
-    /**
-     * Handle the request
-     */
-    $kernel->handle();
-
-    /**
-     * Terminate the kernel
-     */
-    $kernel->terminate();
-} catch (\Exception $e) {
-    echo $e->getMessage() . '<br>';
-    echo '<pre>' . $e->getTraceAsString() . '</pre>';
-}
+/**
+ * Handle the request
+ */
+$kernel->handle();
