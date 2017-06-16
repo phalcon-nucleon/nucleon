562ef2dc36bdca76cdf9940b2ba8689d7322ddec
Ark4ne <gui.allegret@gmail.com>
2017-06-17 01:24:00 +0200

    Update config/compile file with latest refacto

--- config/compile.php
+++ config/compile.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    /* MICRO KERNEL */
-    __DIR__.'/../app/Kernels/Micro/Kernel.php',
-    __DIR__.'/../app/Kernels/Micro/Controllers/MicroController.php',
-
     /* HTTP KERNEL */
-    __DIR__.'/../app/Kernels/Http/Kernel.php',
-    __DIR__.'/../app/Kernels/Http/Controllers/ControllerBase.php',
-    __DIR__.'/../app/Kernels/Http/Controllers/ControllerJson.php',
+    __DIR__ . '/../app/Kernels/Http/Kernel.php',
+    __DIR__ . '/../app/Kernels/Http/Controllers/ControllerBase.php',
+    __DIR__ . '/../app/Kernels/Http/Controllers/ControllerJson.php',
+
+    /* MICRO KERNEL */
+    __DIR__ . '/../app/Kernels/Micro/Kernel.php',
+    __DIR__ . '/../app/Kernels/Micro/Controllers/MicroController.php',
 
     /* HTTP KERNEL - NO-MODULE */
-    __DIR__.'/../app/Kernels/Http/Controllers/HomeController.php',
+    __DIR__ . '/../app/Kernels/Http/Controllers/HomeController.php',
 
     /* HTTP KERNEL - MODULE FRONTEND */
-    __DIR__.'/../app/Kernels/Http/Modules/Frontend/Module.php',
-    __DIR__.'/../app/Kernels/Http/Modules/Frontend/Controllers/IndexController.php',
+    __DIR__ . '/../app/Kernels/Http/Modules/Frontend/Module.php',
+    __DIR__ . '/../app/Kernels/Http/Modules/Frontend/Controllers/IndexController.php',
 ];
