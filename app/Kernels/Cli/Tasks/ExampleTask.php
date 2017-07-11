8f91f4c8cc8f7c6e5a409bf728dc8a2924b87702
Ark4ne <gui.allegret@gmail.com>
2017-07-11 10:40:17 +0200

    Add a showup action task, for present questions work

--- app/Kernels/Cli/Tasks/ExampleTask.php
+++ app/Kernels/Cli/Tasks/ExampleTask.php
@@ -26,7 +26,7 @@ class ExampleTask extends Task
     }
 
     /**
-     * \> php quark some test hello world
+     * \> php quark some:test hello world
      *
      * @argument param_1: the first param
      * @argument param_2: the second param
@@ -36,5 +36,35 @@ class ExampleTask extends Task
     public function testAction(array $params)
     {
         $this->line($this->output->notice(__METHOD__) . ' ' . Decorate::info(implode(' ', $params)));
+    }
+
+    /**
+     * \> php quark show:up
+     */
+    public function showupAction()
+    {
+        $this->line('Hi !');
+        $name = $this->prompt('I\'m quark ! And you, who you are ?');
+
+        $this->line('Nice to meet you ' . $name);
+
+        if ($this->confirm('Are you humain ?', true)) {
+            $this->info('Yes you are !');
+
+            switch ($this->choices('You are a male, a female, or just human ?', ['male', 'female', 'human'], 'human')) {
+                case 'male':
+                case 'female':
+                    $this->line('Ok');
+                    break;
+                case 'human':
+                    $this->line('Ok, i like your thinking way :)');
+                    break;
+            }
+        } else {
+            $this->info('Hey ! you like me');
+        }
+
+        $this->line('');
+        $this->line('Well, by ' . $name);
     }
 }
--- routes/cli.php
+++ routes/cli.php
@@ -15,6 +15,10 @@ use Neutrino\Support\Facades\Router;
 Router::add('some', [
     'task' => \App\Kernels\Cli\Tasks\ExampleTask::class
 ]);
+Router::add('show:up', [
+    'task'    => \App\Kernels\Cli\Tasks\ExampleTask::class,
+    'action'  => 'showup',
+]);
 Router::add('some:test {param_1} {param_2}', [
     'task'    => \App\Kernels\Cli\Tasks\ExampleTask::class,
     'action'  => 'test',
